eeb437a1cd9369c852d696a76bcc08fa37d53f8b
BijanProgrammer <bijaneisapour@gmail.com>
2024-05-25 14:35:23 +0330

    [INIT]

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import {ReactElement} from 'react';
+
+import styles from './page.module.scss';
+
+export default function Home(): ReactElement {
+    return <main className={styles.main}></main>;
+}
